108550b09a16a51bb5264d09b9cc50c67646b3e5
ali <46577578+alinaeemaei@users.noreply.github.com>
2019-05-28 10:54:33 +0430

    Updates

---
+++ precache-manifest.31c09c7d8332012aee68e7a868beeabb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a983bfb28d5ad8809bdfd8bcae51e2",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c07ac39a7f296d94ad45",
+    "url": "/weather/static/css/main.4e1b4ffd.chunk.css"
+  },
+  {
+    "revision": "d730bb0e23f90dd4f61e",
+    "url": "/weather/static/js/2.e7c24e45.chunk.js"
+  },
+  {
+    "revision": "c07ac39a7f296d94ad45",
+    "url": "/weather/static/js/main.5de19ff1.chunk.js"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  }
+]);
